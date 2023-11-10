5ebd5eae76081ee075bb94a6af2bde72bb6bdd25
Tom Gregor <tgregor@ivsoftware.com>
2023-11-10 10:32:38 -0700

    RC

--- securities-list-view/MainForm.cs
+++ securities-list-view/MainForm.cs
@@ -1,6 +1,8 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Windows.Forms;
 using static System.Net.Mime.MediaTypeNames;
@@ -51,25 +53,54 @@ namespace securities_list_view
             _ = benchmark();
         }
 
+        private void setListOfItemsConditionalChecked()
+        {
+            for (int i = 0; i < SecuritiesList.Count; i++) 
+            {
+                var mod = i + 1;
+                SecuritiesList[i].IsChecked = (mod % 4 == 0);
+            }
+        }
+
         /// <summary>
-        ///  Show the dialog, then in 2 seconds set the 
-        ///  checkboxes to true for 3000 items.
+        ///  Show the dialog, wait 2 seconds, then 
+        ///  run conditional set check for 3000 items.
         /// </summary>
         async Task benchmark()
         {
-            Text = DateTime.Now.ToString(@"hh\:mm\:ss tt");
             await Task.Delay(TimeSpan.FromSeconds(2));
+            Text = DateTime.Now.ToString(@"hh\:mm\:ss\.ffff tt");
+            screenshot("screenshot.1.png");
             var stopwatch = Stopwatch.StartNew();
-            foreach (var item in SecuritiesList)
-            {
-                item.IsChecked = true;
-            }
+
+            // Example of a function that will get list
+            // of items that need to be checked.
+            setListOfItemsConditionalChecked();
+
             m_lsvSecurities.Refresh();
             stopwatch.Stop();
-            Text = DateTime.Now.ToString(@"hh\:mm\:ss tt");
+            Text = DateTime.Now.ToString(@"hh\:mm\:ss\.ffff tt");
             await Task.Delay(TimeSpan.FromSeconds(0.5));
             MessageBox.Show($"{stopwatch.ElapsedMilliseconds} ms");
         }
+
+
+
+        private async void screenshot(string fileName)
+        {
+            var imagePath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),
+                Assembly.GetEntryAssembly().GetName().Name,
+                fileName
+            );
+            Directory.CreateDirectory(Path.GetDirectoryName(imagePath));
+            Bitmap bmp = new Bitmap(this.Width, this.Height);
+            DrawToBitmap(bmp, new Rectangle(0, 0, bmp.Width, bmp.Height));
+            bmp.Save(imagePath, ImageFormat.Png);
+            await Task.Delay(TimeSpan.FromSeconds(5));
+            Process.Start(new ProcessStartInfo { FileName = imagePath, UseShellExecute = true,});
+        }
+
         private List<Security> SecuritiesList { get; } = new List<Security>();
     }
     class Security
